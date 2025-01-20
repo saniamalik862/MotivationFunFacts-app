111d733981763ee86f647252dad06e5673b30117
saniamalik862 <sanianawaz805@gmail.com>
2025-01-20 07:44:02 -0800

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Motivation & Fun Facts!</h1>
+      <p>Stay inspired and entertained with our daily dose of motivation and fun facts!</p>
+    </div>
+  );
+}
+
+export default App;
